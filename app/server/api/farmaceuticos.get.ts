67ac2b6bbf27eedf3940310476702c12f35528b0
HordLawk <22248342+HordLawk@users.noreply.github.com>
2024-06-16 04:27:44 -0300

    fix(api): topic name

--- app/server/api/farmaceuticos.get.ts
+++ app/server/api/farmaceuticos.get.ts
@@ -3,7 +3,7 @@ import kafka from '~/server/utils/kafka';
 export default defineEventHandler(async event => {
     const consumer = kafka.consumer({ groupId: 'farmaceuticos' });
     await consumer.connect();
-    await consumer.subscribe({ topics: ['farmaceuticos'], fromBeginning: true });
+    await consumer.subscribe({ topics: ['farmaceutico'], fromBeginning: true });
     await consumer.run({eachMessage: async ({ message }) => console.log(message.value)});
     await consumer.disconnect();
     return {status: 201};
