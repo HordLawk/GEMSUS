cfca02c251d1a2940f6a6077bf46c5d3bbe98f5d
HordLawk <22248342+HordLawk@users.noreply.github.com>
2024-06-16 04:19:23 -0300

    feat: farmaceuticos endpoint

---
+++ app/server/api/farmaceuticos.get.ts
@@ -0,0 +1,10 @@
+import kafka from '~/server/utils/kafka';
+
+export default defineEventHandler(async event => {
+    const consumer = kafka.consumer({ groupId: 'farmaceuticos' });
+    await consumer.connect();
+    await consumer.subscribe({ topics: ['farmaceuticos'], fromBeginning: true });
+    await consumer.run({eachMessage: async ({ message }) => console.log(message.value)});
+    await consumer.disconnect();
+    return {status: 201};
+})
