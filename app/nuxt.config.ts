add8e1b9a51477ca2733a584d3dc557d5349d18c
HordLawk <22248342+HordLawk@users.noreply.github.com>
2024-06-19 16:18:59 -0300

    ref(app): changed home page

--- app/nuxt.config.ts
+++ app/nuxt.config.ts
@@ -1,6 +1,6 @@
 // https://nuxt.com/docs/api/configuration/nuxt-config
 export default defineNuxtConfig({
     devtools: { enabled: true },
-    routeRules: { '/': { redirect: '/estoque-local' } },
+    routeRules: { '/': { redirect: '/medicamentos' } },
     nitro: { experimental: { websocket: true } },
 });
